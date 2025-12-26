ead994e35bf0c2cfdd321a2effad6f142fcca67b
mauryasunil <sunil.pccs@gmail.com>
2025-12-26 17:57:08 +0530

    test2

--- repositories/item_repo.py
+++ repositories/item_repo.py
@@ -1,8 +1,15 @@
 
-from database import SessionLocal
+from database import Session
 from models.item import Item
-class ItemRepository:
-    def add(self,item):
-        s=Session(); s.add(item); s.commit(); s.close()
-    def all(self):
-        s=Session(); r=s.query(Item).all(); s.close(); return r
+
+def add_item(item):
+    s = Session()
+    s.add(item)
+    s.commit()
+    s.close()
+
+def get_items():
+    s = Session()
+    r = s.query(Item).all()
+    s.close()
+    return r
